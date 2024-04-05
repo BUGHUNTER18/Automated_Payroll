4c05426483d9ad76a9154da39a6ea9917a9794a0
APNET02 <163328320+BUGHUNTER18@users.noreply.github.com>
2024-04-05 12:16:16 +0530

    did

--- Payroll/index.php
+++ Payroll/index.php
@@ -23,7 +23,7 @@
 			  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
 			    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 			      <li class="nav-item active">
-			        <a class="nav-link" href="https://github.com/harshraj22/Automated_Payroll">Git <span class="sr-only">(current)</span></a>
+			        <a class="nav-link" href="https://github.com/BUGHUNTER18">Git <span class="sr-only">(current)</span></a>
 			      </li>
 			      <li class="nav-item">
 			        <a class="nav-link" href="#">{$link}</a>
@@ -88,8 +88,8 @@
 		    <footer>
 		        <div class="row">
 		            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-		                <p><u><a href="https://github.com/harshraj22/Automated_Payroll" style="color:black;">Automated Payroll </a></u> is a Registered Website of IIT Dh, Inc. India 
-		                <p class="h6">&copy All right Reversed.<a class="text-green ml-2" href="https://github.com/harshraj22/Automated_Payroll" target="_blank" style="color:black;">Team 5</a></p>
+		                <p><u><a href="https://github.com/BUGHUNTER18" style="color:black;">Automated Payroll </a></u> is a Registered Website of IIT Dh, Inc. India 
+		                <p class="h6">&copy All right Reversed.<a class="text-green ml-2" href="https://github.com/BUGHUNTER18" target="_blank" style="color:black;">Team 5</a></p>
 		            </div>
 		            </hr>
 		        </div>  
